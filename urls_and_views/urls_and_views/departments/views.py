bd335fe9c58ef70fa8c333824b8565dd67f3ab68
Rado-999 <dimitrov.r357@gmail.com>
2024-01-17 04:23:41 +0200

    Adding new paths to project such as CRUD operations(Create,Edit,Delete) and removing to search deparment by name and slug.Now departments can be found only with Id's

--- urls_and_views/urls_and_views/departments/views.py
+++ urls_and_views/urls_and_views/departments/views.py
@@ -3,20 +3,20 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index1(request, *args, **kwargs):
-    return HttpResponse("<h1>Welcome to department1 section</h1>")
-
-
-def index2(request, *args, **kwargs):
-    return HttpResponse("<h1>Welcome to department2 section</h1>")
-
-
-def index3(request, *args, **kwargs):
-    return HttpResponse("<h1>Welcome to department3 section</h1>")
-
-
-def index4(request, *args, **kwargs):
-    return HttpResponse("<h1>Welcome to department4 section</h1>")
+# def index1(request, *args, **kwargs):
+#     return HttpResponse("<h1>Welcome to department1 section</h1>")
+#
+#
+# def index2(request, *args, **kwargs):
+#     return HttpResponse("<h1>Welcome to department2 section</h1>")
+#
+#
+# def index3(request, *args, **kwargs):
+#     return HttpResponse("<h1>Welcome to department3 section</h1>")
+#
+#
+# def index4(request, *args, **kwargs):
+#     return HttpResponse("<h1>Welcome to department4 section</h1>")
 
 
 def welcome_page(request, *args, **kwargs):
@@ -26,8 +26,22 @@ def welcome_page(request, *args, **kwargs):
 def show_department_by_id(request, department_id):
     return HttpResponse(f"<h1>Welcome to department {department_id} section</h1>")
 
+
 def show_deparment_by_str(request, department_name):
     return HttpResponse(f"<h1>Welcome to department {department_name} section</h1>")
 
+
 def show_deparment_by_slug(request, slug):
     return HttpResponse(f"<h1>Welcome to department {slug} section</h1>")
+
+
+def create_department(request):
+    return HttpResponse(f"<h1>Here u can create a new department</h1>")
+
+
+def edit_department(request):
+    return HttpResponse(f"<h1>Here u can edit an existing department</h1>")
+
+
+def delete_department(request):
+    return HttpResponse(f"<h1>Here u can delete an existing department</h1>")
